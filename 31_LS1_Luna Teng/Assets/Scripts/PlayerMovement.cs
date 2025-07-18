95cdf8352e857a0e09ef98c36f90ef0b4233172a
31_Luna Teng <nanatyc1029@gmail.com>
2025-07-18 13:10:56 +0800

    Added player movement

--- 31_LS1_Luna Teng/Assets/Scripts/PlayerMovement.cs
+++ 31_LS1_Luna Teng/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalMovement = Input.GetAxis("Horizontal");
+
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalMovement, 0, verticalMovement);
+
+        transform.Translate(direction * Time.deltaTime * speed);
     }
 }
